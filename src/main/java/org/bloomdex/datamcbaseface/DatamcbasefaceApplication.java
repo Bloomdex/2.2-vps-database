48af543f00d8b013dfd535beda9f0998361e824b
LutzenH <46445220+LutzenH@users.noreply.github.com>
2020-01-17 13:18:21 +0100

    Initial commit

---
+++ src/main/java/org/bloomdex/datamcbaseface/DatamcbasefaceApplication.java
@@ -0,0 +1,13 @@
+package org.bloomdex.datamcbaseface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatamcbasefaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatamcbasefaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/bloomdex/datamcbaseface/DatamcbasefaceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bloomdex.datamcbaseface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatamcbasefaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
