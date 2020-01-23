cb4725514ff64f9999ae1bf7cbfef9952b4390a3
Bart <bartkuiper13@gmail.com>
2020-01-23 23:28:00 +0100

    Made A SSL encrypted server.

---
+++ src/main/java/org/bloomdex/server/Server.java
@@ -0,0 +1,95 @@
+package server;
+
+import javax.net.ssl.*;
+import java.io.*;
+import java.net.*;
+
+public class Server {
+    private static int port = 0;
+    private static String IP;
+    public static boolean PRINT_INFO = false;
+    private static final String[] protocols = new String[] {"TLSv1.3"};
+    private static final String[] cipher_suites = new String[] {"TLS_AES_128_GCM_SHA256"};
+
+    /**
+     * Creates a server thread for every client
+     * @throws IOException throws an exception if:
+     *      something went wrong with creating the server socket
+     *      something went wrong with getting the public IP of the server
+     */
+    private static void createServerThread() throws IOException {
+        SSLServerSocket socket = createServerSocket();
+        if (Server.PRINT_INFO || !Server.PRINT_INFO) {
+            System.out.println("The server started on public IP-address " + getPublicIP());
+            System.out.println("The server started on port " + socket.getLocalPort());
+        }
+        try {
+            while (true) {
+                new ServerThread(socket.accept()).start();
+            }
+        } catch (Exception e) {
+            System.out.printf("exception: %s%n", e.getMessage());
+        }
+        System.out.println("Server stopped.");
+    }
+
+    /**
+     * Creates a SSL server socket to allow encrypted connections
+     *
+     * @return a SSL server socket
+     * @throws IOException throws an exception if something went wrong with creating the SSL server socket
+     */
+    private static SSLServerSocket createServerSocket() throws IOException {
+        SSLServerSocket socket = (SSLServerSocket) SSLServerSocketFactory.getDefault().createServerSocket(port);
+        socket.setEnabledProtocols(protocols);
+        socket.setEnabledCipherSuites(cipher_suites);
+        return socket;
+    }
+
+    /**
+     * Gets the public IP of the server from http://checkip.amazonaws.com
+     *
+     * @return returns the public IP of the server
+     * @throws IOException throws an exception if something went wrong with the request for the public ip of the server on http//checkip.amazonaws.com
+     */
+    public static String getPublicIP() throws IOException {
+        URL whatismyip = new URL("http://checkip.amazonaws.com");
+        BufferedReader in = new BufferedReader(new InputStreamReader(whatismyip.openStream()));
+        return in.readLine();
+    }
+
+    /**
+     * Returns the open port of the server
+     *
+     * @return Returns the open port of the server
+     */
+    public static int getPort() {
+        return port;
+    }
+
+    /**
+     * Returns the IP of the server
+     *
+     * @return returns the IP of the server
+     */
+    public static String getIP() {
+        return IP;
+    }
+
+    /**
+     *  The main function witch starts the server
+     *
+     * @param args if verbose is given as an arg the server will print his port and IP address
+     * @throws IOException throws an exception when something went wrong with creating a server thread
+     */
+    public static void main(String[] args) throws IOException {
+        if(args.length > 0) {
+            if(args[0].equals("--verbose") || args[0].equals("-v"))
+                PRINT_INFO = true;
+        }
+
+        System.setProperty("javax.net.ssl.keyStore", "keystore");
+        System.setProperty("javax.net.ssl.keyStorePassword", "passphrase");
+        createServerThread();
+    }
+}
---
+++ src/main/java/org/bloomdex/server/ServerThread.java
@@ -0,0 +1,59 @@
+package server;
+
+import java.io.*;
+import java.net.*;
+
+public class ServerThread extends Thread {
+    private final Socket socket;
+    private DataInputStream dataInputStream;
+    private byte[] data;
+
+    /**
+     * The constructor which sets the socket as a variable
+     *
+     * @param socket the socket which receives the data
+     */
+    ServerThread(Socket socket){
+        this.socket = socket;
+        System.out.println("new client accepted");
+    }
+
+    /**
+     * Runs the main loop from the thread which is receiving the data
+     */
+    public void run() {
+        try {
+            dataInputStream = new DataInputStream(socket.getInputStream());
+            while (!socket.isClosed()){
+                int length = dataInputStream.readInt();
+                if(length>0) {
+                    data = new byte[length];
+                    dataInputStream.readFully(data, 0, data.length);
+
+                    // need to be done!!!
+                    // notify the database code there is new data that can be inserted into the database
+                }
+
+                // prints the incoming data
+                /*
+                for (int x = 0; x <= length - 1; x++) {
+                    System.out.print((char)data[x]);
+                }
+                System.out.print("\n");
+                 */
+            }
+        } catch (IOException e) {
+            System.out.printf("exception: %s%n", e.getMessage());
+        }
+    }
+
+    /**
+     * Returns the incoming data
+     *
+     * @return returns the incoming data
+     */
+    public byte[] getData() {
+        return data;
+    }
+}
+
