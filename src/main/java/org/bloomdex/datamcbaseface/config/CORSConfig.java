503044967192023e87fa306140a25ece96d322e6
LutzenH <46445220+LutzenH@users.noreply.github.com>
2020-02-02 15:02:06 +0100

    Fix bug where application/xml would be returned by default

--- src/main/java/org/bloomdex/datamcbaseface/config/CORSConfig.java
+++ src/main/java/org/bloomdex/datamcbaseface/config/CORSConfig.java
@@ -1,6 +1,8 @@
 package org.bloomdex.datamcbaseface.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,9 +10,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class CORSConfig implements WebMvcConfigurer {
+    /**
+     * Custom CORS to allow users to perform PUT, DELETE and OPTIONS
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("GET", "POST","PUT", "DELETE", "OPTIONS");
+    }
+
+    /**
+     * Set the default return type at the client to .JSON instead of .XML
+     */
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.APPLICATION_JSON);
     }
 }
